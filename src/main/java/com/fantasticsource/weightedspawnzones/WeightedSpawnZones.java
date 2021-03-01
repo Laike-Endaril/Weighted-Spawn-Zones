6f35295ddf23f9aef768ab66b5f67d20678065a1
Kyle Koder <silvertallon@gmail.com>
2021-02-28 21:05:35 -0800

    Better testing coords.  Basic tests are passing so far

--- src/main/java/com/fantasticsource/weightedspawnzones/WeightedSpawnZones.java
+++ src/main/java/com/fantasticsource/weightedspawnzones/WeightedSpawnZones.java
@@ -66,7 +66,7 @@ public class WeightedSpawnZones
             weightedEntity.entityNBT = JsonToNBT.getTagFromJson("{PersistenceRequired:1b,Attributes:[{Base:20.0d,Name:\"generic.maxHealth\"},{Base:0.0d,Modifiers:[{UUIDMost:5852893299261065939L,UUIDLeast:-9104073199294987705L,Amount:0.036573104176229713d,Operation:0,Name:\"Randomspawnbonus\"}],Name:\"generic.knockbackResistance\"},{Base:0.23000000417232513d,Name:\"generic.movementSpeed\"},{Base:2.0d,Name:\"generic.armor\"},{Base:0.0d,Name:\"generic.armorToughness\"},{Base:1.0d,Name:\"forge.swimSpeed\"},{Base:35.0d,Modifiers:[{UUIDMost:1016541881440161498L,UUIDLeast:-5301211752535089558L,Amount:0.05163393930534082d,Operation:1,Name:\"Randomspawnbonus\"}],Name:\"generic.followRange\"},{Base:3.0d,Name:\"generic.attackDamage\"},{Base:0.014275049799433781d,Name:\"zombie.spawnReinforcements\"}],Invulnerable:1b,HandDropChances:[0.085f,0.085f],id:\"minecraft:husk\",Health:20.0f,Air:300s,OnGround:1b,Rotation:[122.1131f,0.0f],HandItems:[{},{}],ArmorDropChances:[1.085f,0.085f,0.085f,0.085f],Pos:[1005.5d,65.0d,-1.5d],Fire:-1s,ArmorItems:[{},{},{},{}],}");
 
             CWeightedSpawnZone zone = new CWeightedSpawnZone();
-            BlockPos pos = new BlockPos(0, 80, 0);
+            BlockPos pos = new BlockPos(1000, 200, 0);
             zone.setBounds(pos, pos);
 
             weightedEntity.weightedZones.add(zone);
